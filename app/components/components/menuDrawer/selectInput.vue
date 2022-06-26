94874280e0e879f3b6b35aa8975298bdb77dc2b3
Legnar Rangel <legnarrangel1995@gmail.com>
2022-06-26 16:09:23 -0300

    integrando Api rest

---
+++ app/components/components/menuDrawer/selectInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <StackLayout marginTop="4" marginLeft="24" marginRight="24"> 
+       <TextField  tabTextFontSize="50" class="textbox" height="38"  v-model=".email" hint="Email"
+                    backgroundColor="white" borderRadius="10" marginBottom="6"/>
+  </StackLayout>
+</template>
+<script>
+export default {
+  props: {
+     data: {
+      type: Object,
+      default: {},
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+   
+  },
+};
+</script>
+
+<style scoped>
+.home-panel {
+  vertical-align: center;
+  font-size: 20;
+  margin: 15;
+}
+
+.description-label {
+  margin-bottom: 15;
+}
+</style>
